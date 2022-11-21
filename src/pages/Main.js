d2b2010fd1e117fe45fd80f6c4301463704d84d7
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-11-22 01:07:34 +0300

    asssets added, main page started

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,7 +4,29 @@ class Main extends React.Component {
   render() {
     return (
       <>
-        <h1>Main</h1>
+        <header className="bg-slate-300/20 flex justify-center ">
+          <strong className="text-center">Fitness Tracker</strong>
+        </header>
+
+        <div id="slogan">
+          <ul>
+            <li>
+              <strong>TRAIN SMARTER</strong>
+            </li>
+            <li>
+              <strong>ACHIEVE FASTER</strong>
+            </li>
+          </ul>
+
+          <div id="buttons">
+            <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border border-blue-700 hover:border-blue-500 rounded">
+              Button
+            </button>
+            <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-bold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              Button
+            </button>
+          </div>
+        </div>
       </>
     );
   }
