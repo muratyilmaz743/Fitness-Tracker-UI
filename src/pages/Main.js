442c9ea391a3311fa52c9aadd74585f5210a0e88
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-11-23 08:40:32 +0300

    landing page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,33 +6,35 @@ class Main extends React.Component {
   render() {
     return (
       <>
-        <header className="bg-slate-300/20 flex justify-center ">
-          <strong className="text-center">Fitness Tracker</strong>
-        </header>
+        <div className="landingPage">
+          <header className="bg-slate-300/20 flex justify-center ">
+            <strong className="align-middle inline text-5xl text-[#1746A2]">Fitness Tracker</strong>
+          </header>
 
-        <div id="slogan">
-          <ul>
-            <li>
-              <strong>TRAIN SMARTER</strong>
-            </li>
-            <li>
-              <strong>ACHIEVE FASTER</strong>
-            </li>
-          </ul>
+          <div id="slogan">
+            <ul>
+              <li>
+                <strong className="text-[#1746A2] mix-blend-luminosity">TRAIN SMARTER</strong>
+              </li>
+              <li>
+                <strong className="text-[#5F9DF7] mix-blend-hue">ACHIEVE FASTER</strong>
+              </li>
+            </ul>
 
-          <div id="buttons">
-            <a
-              class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border border-blue-700 hover:border-blue-500 rounded"
-              href="login"
-            >
-              Login
-            </a>
-            <a
-              class="bg-transparent hover:bg-blue-500 text-blue-700 font-bold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-              href="register"
-            >
-              Register
-            </a>
+            <div id="buttons">
+              <a
+                class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border border-blue-700 hover:border-blue-500 rounded"
+                href="login"
+              >
+                Login
+              </a>
+              <a
+                class="bg-transparent hover:bg-blue-500 text-blue-700 font-bold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                href="register"
+              >
+                Register
+              </a>
+            </div>
           </div>
         </div>
       </>
