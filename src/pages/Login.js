1539246eb93b74122ac4f2f77ebfd6496aaa2e23
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-11-24 08:44:57 +0300

    login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,16 +5,9 @@ class Login extends React.Component {
   render() {
     return (
       <>
-        <section class="h-screen">
+        <section class="h-screen loginPage">
           <div class="px-6 h-full text-gray-800">
-            <div class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
-              <div class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                  class="w-full"
-                  alt="Sample image"
-                />
-              </div>
+            <div class="flex xl:justify-center lg:justify-between justify-center flex-wrap h-full g-6">
               <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
                 <form>
                   <div class="flex flex-row items-center justify-center lg:justify-start">
