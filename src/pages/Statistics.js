b3816f0ab2716b3a0a6342b89d8415f3d362738d
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-12-04 19:01:16 +0300

    login and register completed

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,9 +1,42 @@
 import { Component } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 class Stats extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weight: "",
+      fat: "",
+    };
+    this.daychanged = this.daychanged.bind(this);
+  }
+
+  daychanged() {
+    this.setState({ weight: "123", fat: "11" });
+  }
+
   render() {
     return (
-        <p>Statistics</p>
+      <>
+        <html>
+          <body>
+            <div id="statisticsPage">
+              <section>
+                <div class="calendar">
+                  <Calendar onChange={this.daychanged}/>
+                </div>
+
+                <div class="attributes" id="box">
+                  <label>Weight: {this.state.weight}</label> <br />
+                  <br />
+                  <label> Fat Rate: {this.state.fat}</label>
+                </div>
+              </section>
+            </div>
+          </body>
+        </html>
+      </>
     );
   }
 }
--- src/pages/TeacherList.js
+++ src/pages/TeacherList.js
@@ -2,39 +2,86 @@ import { Component } from "react";
 import React from "react";
 import ReactModal from "react-modal";
 import TeacherDetail from "../Components/teacherDetailSidebar";
+import Rating from "../Components/ratingStars";
 
 class TeacherList extends Component {
   constructor() {
     super();
     this.state = {
       showModal: false,
+      content: ""
     };
 
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
   }
 
-  handleOpenModal() {
-    this.setState({ showModal: true });
+  handleOpenModal(content) {
+    this.setState({ showModal: true, content:content });
   }
 
   handleCloseModal() {
-    this.setState({ showModal: false });
+    this.setState({ showModal: false});
   }
 
   render() {
     return (
-      <div>
-        <button onClick={this.handleOpenModal}>Trigger Modal</button>
-        <ReactModal
-          className="Modal"
-          isOpen={this.state.showModal}
-          contentLabel="Minimal Modal Example"
-        >
-          <TeacherDetail />
-          <button onClick={this.handleCloseModal}>Close Modal</button>
-        </ReactModal>
-      </div>
+      <>
+        <div className="container lg:mx-auto mt-8">
+          <strong className="text-2xl">
+            Find the best teacher for yourself
+          </strong>
+          <div id="teacherBox" className="box-border h-56 w-56 p-4 border-4 m-6">
+            <div class="mb-4">
+              <img
+                src="https://mdbootstrap.com//img/Photos/Square/1.jpg"
+                class="max-w-full h-28 rounded-full"
+                alt=""
+              />
+            </div>
+            <Rating />
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-2" onClick={() => this.handleOpenModal('a')}>
+              Trainer Name
+            </button>
+          </div>
+          <div id="teacherBox" className="box-border h-56 w-56 p-4 border-4 m-6">
+            <div class="mb-4">
+              <img
+                src="https://mdbootstrap.com//img/Photos/Square/1.jpg"
+                class="max-w-full h-28 rounded-full"
+                alt=""
+              />
+            </div>
+            <Rating />
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-2" onClick={() => this.handleOpenModal('b')}>
+              Trainer Name
+            </button>
+          </div>
+          <div id="teacherBox" className="box-border h-56 w-56 p-4 border-4 m-6">
+            <div class="mb-4">
+              <img
+                src="https://mdbootstrap.com//img/Photos/Square/1.jpg"
+                class="max-w-full h-28 rounded-full"
+                alt=""
+              />
+            </div>
+            <Rating />
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-2" onClick={() => this.handleOpenModal('c')}>
+              Trainer Name
+            </button>
+          </div>
+          <div>
+            <ReactModal
+              className="Modal"
+              isOpen={this.state.showModal}
+            >
+              <TeacherDetail />
+              <span>{this.state.content}</span>
+              <button onClick={this.handleCloseModal}>Close</button>
+            </ReactModal>
+          </div>
+        </div>
+      </>
     );
   }
 }
