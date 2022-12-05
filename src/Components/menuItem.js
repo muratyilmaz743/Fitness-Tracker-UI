14f939d047054850817f5eae5dc59ea0abadd62e
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-12-05 13:48:40 +0300

    menu

--- src/Components/menuItem.js
+++ src/Components/menuItem.js
@@ -4,8 +4,8 @@ function MenuItem({ url, name, svg }) {
   return (
     <>
       <div>
-        <div className="md:container md:mx-auto flex justify-items-center">
-          <a href={url} className="box-border h-56 w-56 p-4 border-4 m-6 flex items-end hover:bg-sky-100 items-center justify-items-center ">
+        <div className="md:container md:mx-auto flex justify-items-center opacity-75">
+          <a href={url} className="border-double box-border h-56 w-56 p-4 border-8 m-6 flex items-end hover:bg-sky-100 items-center justify-items-center rounded-xl bg-zinc-50">
             <div className="absolute pb-20">{svg}</div>
             <div>
               <p className="text-4xl pt-8">
