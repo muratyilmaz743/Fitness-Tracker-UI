f365e6f0d3016508663aeb16241db0ee252eb828
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-12-14 09:17:46 +0300

    calendar page

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -18,21 +18,24 @@ class Stats extends Component {
   }
 
   render() {
+    const current = new Date();
+    const date = `${current.getDate()}/${
+      current.getMonth() + 1
+    }/${current.getFullYear()}`;
     return (
       <>
         <html>
           <body>
             <div id="statisticsPage">
-              <section>
+              <section className="">
                 <div class="calendar">
                   <Calendar onClickDay={() => this.daychanged(this)} />
                 </div>
 
-                <div class="attributes">
-                  <label class="calendarDays">Month/Day/Year</label>
-                  <br />
-                  <ul>
-                    <li id="attribute">
+                <div class="attributes mt-20 container-md h-screen bg-gradient-to-r from-cyan-50 to-blue-50 rounded-3xl text-center">
+                  <span class="calendarDays text-5xl text-center top-16 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 font-bold">{date}</span>
+                  <ul className="absolute top-1/4">
+                    <li id="attribute" >
                       <label class="ratios">Weight: {this.state.weight} </label>
                     </li>
                     <li id="attribute">
