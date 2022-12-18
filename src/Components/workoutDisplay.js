f66b0c3f3aa048ce211f93f7c02c67d094c9e4a4
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-12-19 02:12:12 +0300

    egzersizler

--- src/Components/workoutDisplay.js
+++ src/Components/workoutDisplay.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-function WorkoutDisplay({ url, name}) {
+function WorkoutDisplay({ url, name }) {
   return (
     <>
-      <div
-        className="h-56 w-56 p-4 border-4 border-black m-6 rounded-xl min-w-[14rem]"
-      >
+      <div className="h-56 w-56 p-4 border-4 border-black m-6 rounded-xl min-w-[14rem] bg-white">
         <div class="mb-4 h-full">
           <p class="text-center bg-blue-200 text-black font-bold py-2 px-4 rounded-full mt-2">
             {name}
           </p>
-          <img src={url} alt="gif of move"  width="250" />
+          <div className="w-full flex justify-center">
+            <img className="h-28" src={url} alt="gif of move" />
+          </div>
         </div>
       </div>
     </>
--- src/pages/Workouts.js
+++ src/pages/Workouts.js
@@ -6,14 +6,15 @@ import WorkoutDisplay from "../Components/workoutDisplay";
 class Workouts extends Component {
   state = {
     workoutList: [],
+    title: "Bütün Egzersizler",
   };
 
   constructor() {
     super();
     GetAllExercises().then((res) =>
       res.json().then((result) => {
-        let workout = [...this.state.workoutList];
-        for (let i = 0; i < 20; i++) {
+        let workout = [];
+        for (let i = 0; i < 15; i++) {
           workout.push(result[i]);
         }
         this.setState({ workoutList: workout });
@@ -21,34 +22,52 @@ class Workouts extends Component {
     );
   }
 
+  getActiveFilter(filter) {
+    switch (filter) {
+      case "abs":
+        this.setState({ title: "Karın Egzersizleri" });
+        break;
+      case "lats":
+        this.setState({ title: "Sırt Egzersizleri" });
+        break;
+      case "pectorals":
+        this.setState({ title: "Göğüs Egzersizleri" });
+        break;
+      default:
+        this.setState({ title: "Bütün Egzersizler" });
+    }
+  }
+
   allExercises = () => {
     GetAllExercises().then((res) =>
       res.json().then((result) => {
-        let move = [...this.state.workoutList];
-        for (let i = 0; i < 5; i++) {
+        let move = [];
+        for (let i = 0; i < 15; i++) {
           move.push(result[i]);
         }
         this.setState({ workoutList: move });
       })
     );
+    this.getActiveFilter();
   };
 
   changeFilter = (filter) => {
     GetExercise(filter).then((res) =>
       res.json().then((result) => {
-        let move = [...this.state.workoutList];
-        for (let i = 0; i < 5; i++) {
+        let move = [];
+        for (let i = 0; i < 15; i++) {
           move.push(result[i]);
         }
         this.setState({ workoutList: move });
       })
     );
+    this.getActiveFilter(filter);
   };
 
   render() {
     return (
       <div className="container mx-auto px-10">
-        <div className="container-md h-screen bg-gradient-to-r from-cyan-50 to-blue-50 rounded-3xl">
+        <div className="container-md h-full bg-gradient-to-r from-cyan-50 to-blue-50 rounded-3xl m-5">
           <div className="text-left border-black-500 p-4 w-full">
             <section className="text-center">
               <FilterButton name={"Hepsi"} fire={() => this.allExercises()} />
@@ -58,14 +77,14 @@ class Workouts extends Component {
               />
               <FilterButton
                 name={"Sırt"}
-                fire={() => this.changeFilter("back")}
+                fire={() => this.changeFilter("lats")}
               />
               <FilterButton
-                name={"Kol"}
-                fire={() => this.changeFilter("arms")}
+                name={"Göğüs"}
+                fire={() => this.changeFilter("pectorals")}
               />
             </section>
-            <div class="bg-clip-text text-5xl p-4">Workouts</div>
+            <div class="bg-clip-text text-5xl p-4">{this.state.title}</div>
             <div className="grid grid-cols-5 gap-4 justify-items-center">
               {this.state.workoutList.map((workout) => {
                 return (
