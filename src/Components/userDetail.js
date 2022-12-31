3e96c8d672354d13536cc053f68536756fc78ac1
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-12-31 15:05:00 +0300

    teacher listing

--- src/Components/userDetail.js
+++ src/Components/userDetail.js
@@ -1,29 +1,42 @@
 import React from "react";
 import "../css/userRegister.css";
 import { setUserDetails } from "../services/userService";
+import { GetAllTeachers } from "../services/teacherService";
+
+var teachers;
 
 function setUser() {
   setUserDetails({
-    fullName: document.getElementById('grid-first-name').value,
-    emailAddress: document.getElementById('grid-email').value,
-    phoneNumber: document.getElementById('grid-phone').value,
-    gender: parseInt(document.getElementById('grid-gender').value),
+    fullName: document.getElementById("grid-first-name").value,
+    emailAddress: document.getElementById("grid-email").value,
+    phoneNumber: document.getElementById("grid-phone").value,
+    gender: parseInt(document.getElementById("grid-gender").value),
     age: 21,
-    height: parseInt(document.getElementById('grid-height').value),
-    weight: parseInt(document.getElementById('grid-Weight').value),
+    height: parseInt(document.getElementById("grid-height").value),
+    weight: parseInt(document.getElementById("grid-Weight").value),
     bodyMassIndex: null,
-    neckGirth: parseInt(document.getElementById('grid-Neck').value),
-    hipCircumference: parseInt(document.getElementById('grid-Hip').value),
-    garth: parseInt(document.getElementById('grid-Garth').value),
-    isSmoke: parseInt(document.getElementById('grid-Smoke').value),
+    neckGirth: parseInt(document.getElementById("grid-Neck").value),
+    hipCircumference: parseInt(document.getElementById("grid-Hip").value),
+    garth: parseInt(document.getElementById("grid-Garth").value),
+    isSmoke: parseInt(document.getElementById("grid-Smoke").value),
     sportDescription: "No",
     basalMetabolism: null,
-    target: document.getElementById('grid-Target').value,
-    user: { id: localStorage.getItem('currentUser') },
+    target: document.getElementById("grid-Target").value,
+    user: { id: localStorage.getItem("currentUser") },
   });
 }
 
 export default function userDetail() {
+  GetAllTeachers().then((res) =>
+  res.json().then((teachers) => {
+    for (let i = 0; i < 20; i++) {
+      if(teachers[i].is_active == 1) {
+        var teacherBox = document.getElementById('grid-teacher');
+        teacherBox.options[teacherBox.options.length] = new Option(teachers[i].id, teachers[i].fullName);
+      }
+    }
+  })
+);
   return (
     <div className="w-full h-screen flex justify-center items-center style-form-wrapper">
       <form>
@@ -245,6 +258,31 @@ export default function userDetail() {
             >
               Send Message
             </button>
+          </div>
+
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-teacher"
+            >
+              Your Teacher?
+            </label>
+            <div className="relative">
+              <select
+                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-teacher"
+              >
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg
+                  className="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </div>
           </div>
 
           {/* <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
