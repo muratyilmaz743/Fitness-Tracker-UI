2ecef20f5d215ce89ab39bf42522c1add112836d
sinemturkcu <sinemturkcu@gmail.com>
2023-01-01 20:31:15 +0300

    Teacher sidebar edited

--- src/pages/TeacherList.js
+++ src/pages/TeacherList.js
@@ -101,49 +101,64 @@ class TeacherList extends Component {
           </div>
         </div>
         <div>
-          <ReactModal className="Modal" isOpen={this.state.showModal}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="absolute top-0 right-0 h-16 w-16 cursor-pointer"
-              onClick={this.handleCloseModal}
+          <div className="w-full flex justify-center">
+            <ReactModal
+              className="Modal overflow-y-auto h-50"
+              isOpen={this.state.showModal}
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="absolute top-0 right-0 h-16 w-16 cursor-pointer"
+                onClick={this.handleCloseModal}
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
 
-            <div class="mb-4 h-28">
-              <img
-                src={this.state.imgUrl}
-                class="max-w-full h-64 rounded-full absolute left-24 top-14"
-                alt=""
-              />
-            </div>
-
-            <div className="personal-info absolute top-96 left-12 text-2xl">
-              Tracker Informations <br></br>
-              <i> Name: {this.state.name}</i>
-              <div>Experience Year: {this.state.experienceYear}</div>
-              <div>Description: {this.state.description}</div>
-              <div>
-                <a href={this.state.mailTo}>
-                  <button
-                    class="bg-orange-500 text-white active:bg-orange-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    Send Mail
-                  </button>
-                </a>
+              <div class="mb-4 h-28">
+                <img
+                  src={this.state.imgUrl}
+                  class="max-w-full h-64 rounded-full absolute left-24 top-14"
+                  alt=""
+                />
               </div>
-              <div>{this.state.phone}</div>
-            </div>
-          </ReactModal>
+
+              <div className="personal-info absolute top-96 left-12 text-l">
+                <div className="text-2xl">
+                  <strong>Tracker Informatıon</strong>
+                </div>
+                <i>
+                  {" "}
+                  <strong>Name:</strong> {this.state.name}
+                </i>
+                <div>
+                  <strong>Experience Year: </strong>
+                  {this.state.experienceYear}
+                </div>
+                <div>
+                  <strong>Description:</strong> {this.state.description}
+                </div>
+                <div>
+                  <a href={this.state.mailTo}>
+                    <button
+                      class="bg-orange-500 text-white active:bg-orange-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 mt-5"
+                      type="button"
+                    >
+                      Send Mail
+                    </button>
+                  </a>
+                </div>
+                <div>{this.state.phone}</div>
+              </div>
+            </ReactModal>
+          </div>
           <Footer />
         </div>
       </>
