a725aa1651ef77fab1f2ccf9b9e2eac4040c4624
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2023-01-02 00:22:02 +0300

    trackers students fix

--- src/pages/TrackersStudents.js
+++ src/pages/TrackersStudents.js
@@ -1,47 +1,62 @@
 import React from "react";
+import { Component } from "react";
 import Students from "../Components/trackersStudents";
 import { GetMyUsers } from "../services/userService";
 
-export default function TrackersStudents() {
-  let trackersStudents = [];
-  const trackerId = localStorage.getItem("currentUser");
+class TrackersStudents extends Component {
+  state = {
+    trackersStudents: [],
+  };
 
-  GetMyUsers(trackerId).then((res) =>
-    res.json().then((students) => {
-      for (const student of students) {
-        trackersStudents.push(student);
-      }
-    })
-  );
+  constructor() {
+    super();
 
-  return (
-    <div>
-      <div className="container mx-auto px-40">
-        <strong className="text-2xl mb-[4rem]">My Student List</strong>
+    const trackerId = localStorage.getItem("currentUser");
+
+    GetMyUsers(trackerId).then((res) =>
+      res.json().then((students) => {
+        let p = [...this.state.trackersStudents];
+        for (let i = 0; i < students.length; i++) {
+          p.push(students[i]);
+        }
+        this.setState({ trackersStudents: p });
+      })
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container mx-auto px-40">
+          <strong className="text-2xl mb-[4rem]">My Student List</strong>
+        </div>
+        <div class="px-40 grid grid-cols-3 gap-3 justify-items-center">
+          {this.state.trackersStudents.map((student) => {
+            var realGender = student.gender === true ? "Male" : "Female";
+
+            return (
+              <>
+                <Students
+                  fullName={student.fullName}
+                  weight={student.weight}
+                  height={student.height}
+                  garth={student.garth}
+                  hip={student.hipCircumference}
+                  neck={student.neckGirth}
+                  basal={student.basalMetabolism}
+                  massIndex={student.bodyMassIndex}
+                  smoking={student.isSmoke}
+                  sportDesc={student.sportDescription}
+                  target={student.target.value}
+                  gender={realGender}
+                />
+              </>
+            );
+          })}
+        </div>
       </div>
-      <div class="px-40 grid grid-cols-3 gap-3 justify-items-center">
-        {trackersStudents.map((student) => {
-          var realGender = student.gender === true ? "Male" : "Female"
-          return (
-            <>
-              <Students
-                fullName={student.fullName}
-                weight={student.weight}
-                height={student.height}
-                garth={student.garth}
-                hip={student.hipCircumference}
-                neck={student.neckGirth}
-                basal={student.basalMetabolism}
-                massIndex={student.bodyMassIndex}
-                smoking={student.isSmoke}
-                sportDesc={student.sportDescription}
-                target={student.target.value}
-                gender={realGender}
-              />
-            </>
-          );
-        })}
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default TrackersStudents;
